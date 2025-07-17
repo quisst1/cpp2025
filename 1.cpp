7bee2af3bb6ac1166e6f54b2bb4e66cd7c9826eb
quisst1 <artem20051019@gmail.com>
2025-07-18 02:07:00 +0300

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <clocale> 
+#include <Windows.h> 
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    SetConsoleCP(1251);       
+    SetConsoleOutputCP(1251); 
+
+    double number;
+    cout << "Введите число: ";
+    cin >> number;
+
+    int integerPart = static_cast<int>(number);
+    double fractionalPart = number - integerPart;
+    double roundedValue = round(number);
+
+    cout << "Целая часть: " << integerPart << endl;
+    cout << "Дробная часть: " << fixed << setprecision(6) << fractionalPart << endl;
+    cout << "Округлённое значение: " << roundedValue << endl;
+
+    return 0;
+}
