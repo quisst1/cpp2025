63ddbbfabde0f0bdfe1321b92bc29cb03904afb1
quisst1 <artem20051019@gmail.com>
2025-07-18 13:43:27 +0300

    Add files via upload

---
+++ 3.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <clocale>
+#ifdef _WIN32
+#include <Windows.h>
+#endif
+
+using namespace std;
+
+int computeGCD(int a, int b) {
+    while (a != b) {
+        if (a > b) {
+            a = a - b;
+        }
+        else {
+            b = b - a;
+        }
+    }
+    return a;
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+#ifdef _WIN32
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+#endif
+
+    char choice;
+    do {
+        int a, b;
+
+        cout << "Введите два натуральных числа: ";
+        cin >> a >> b;
+
+        if (a <= 0 || b <= 0) {
+            cout << "Числа должны быть натуральными!" << endl;
+        }
+        else {
+            int gcd = computeGCD(a, b);
+            cout << "НОД: " << gcd << endl;
+        }
+
+        cout << "Продолжить? (y/n): ";
+        cin >> choice;
+    } while (choice == 'y' || choice == 'Y');
+
+    return 0;
+}
