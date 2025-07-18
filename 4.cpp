00007438dc0d17401b97ad3ccdc7eaed7799d39d
quisst1 <artem20051019@gmail.com>
2025-07-18 13:51:37 +0300

    Add files via upload

---
+++ 4.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <clocale>
+#ifdef _WIN32
+#include <Windows.h>
+#endif
+using namespace std;
+
+double calculateY(double x, int terms) {
+    double result = 1.0;
+    double term = 1.0;
+
+    for (int n = 1; n <= terms; ++n) {
+        term *= x / n;
+        result += term;
+    }
+
+    return result;
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+#ifdef _WIN32
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+#endif
+
+    double x;
+    cout << "Введите значение x: ";
+    cin >> x;
+
+    cout << fixed << setprecision(5);
+    cout << "Точное значение e^x: " << exp(x) << endl << endl;
+
+    int terms[] = { 5, 10, 15, 20, 25 };
+
+    for (int n : terms) {
+        double y = calculateY(x, n);
+        cout << "Слагаемых: " << setw(2) << n << "  Y(x) = " << y << endl;
+    }
+
+    return 0;
+}
